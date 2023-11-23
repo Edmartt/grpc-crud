9d2fcfaf70bafa7c8deb21295e68e07745476c37
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-23 03:10:07 -0500

    feat: adds http code for sending data for testing

---
+++ pkg/client/http/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/edmartt/grpc-test/internal/person/models"
+	pb "github.com/edmartt/grpc-test/internal/person/protos/bin"
+	"github.com/edmartt/grpc-test/pkg/client"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+type createdResponse struct {
+	Response string `json:"response"`
+}
+
+func getPerson(context *gin.Context) {
+	id := context.Param("id")
+
+	requestPB := &pb.PersonRequest{
+		Id: id,
+	}
+
+	response := client.ReadPerson(requestPB)
+	log.Println("response client grpc: ", response)
+
+	context.JSON(http.StatusOK, response)
+}
+
+func postPerson(context *gin.Context) {
+	personModel := models.Person{}
+	personProtoModel := &pb.Person{}
+
+	err := context.BindJSON(&personModel)
+
+	personProtoModel.Name = personModel.FirstName
+	personProtoModel.LastName = personModel.LastName
+	personProtoModel.Email = personModel.Email
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	response := client.CreatePerson(personProtoModel)
+	created := createdResponse{
+		Response: response,
+	}
+
+	context.JSON(http.StatusOK, created)
+}
+
+func setPersonRoutes(router *gin.RouterGroup) {
+	router.POST("/person", postPerson)
+	router.GET("/person/:id", getPerson)
+}
+
+func setRouter() *gin.Engine {
+	router := gin.Default()
+	apiGroup := router.Group("/api/v1")
+	setPersonRoutes(apiGroup)
+
+	return router
+}
+
+func start(port string) {
+	router := setRouter()
+	router.Run(":" + port)
+}
+
+func main() {
+	godotenv.Load()
+	start("8080")
+}
