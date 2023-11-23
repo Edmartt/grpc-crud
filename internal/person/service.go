bacaabf243facab00d88a2a89cb2469b0f2da73e
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-23 17:37:48 -0500

    feat: adds Delete gRPC method

--- internal/person/service.go
+++ internal/person/service.go
@@ -2,6 +2,7 @@ package person
 
 import (
 	"context"
+	"log"
 
 	"github.com/edmartt/grpc-test/internal/person/data"
 	"github.com/edmartt/grpc-test/internal/person/models"
@@ -50,4 +51,31 @@ func (s *Service) Get(ctx context.Context, request *pb.PersonRequest) (*pb.Perso
 	}
 
 	return personResponse, nil
+}
+
+func (s *Service) Delete(ctx context.Context, request *pb.DeletePersonRequest) (*pb.DeletePersonResponse, error) {
+	id := request.Id
+	queryPerson, err := dataAccess.Read(id)
+	log.Println("id service: ", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if queryPerson.ID == "" {
+		return &pb.DeletePersonResponse{
+			Id:     id,
+			Status: "not found",
+		}, nil
+	}
+
+	dataAccess.Delete(queryPerson)
+
+	delPersonResponse := &pb.DeletePersonResponse{
+		Id:     queryPerson.ID,
+		Status: "deleted",
+	}
+
+	return delPersonResponse, nil
+
 }
