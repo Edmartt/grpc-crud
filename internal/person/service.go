b1b7abe58b3ac6d4e79849c334ef27a7de337ca9
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-24 15:16:45 -0500

    chore: updates service calls to grpc methods

--- internal/person/service.go
+++ internal/person/service.go
@@ -2,7 +2,6 @@ package person
 
 import (
 	"context"
-	"log"
 
 	"github.com/edmartt/grpc-test/internal/person/data"
 	"github.com/edmartt/grpc-test/internal/person/models"
@@ -20,22 +19,22 @@ type Service struct {
 	pb.UnimplementedPersonServiceServer
 }
 
-func (s *Service) Create(ctx context.Context, person *pb.Person) (*pb.PersonCreatedResponse, error) {
+func (s *Service) Create(ctx context.Context, person *pb.Person) (*pb.CreatePersonResponse, error) {
 	id := uuid.New().String()
 
 	newPerson := &models.Person{
 		ID:        id,
-		FirstName: person.Name,
+		FirstName: person.FirstName,
 		LastName:  person.LastName,
 		Email:     person.Email,
 	}
 
 	status := dataAccess.Create(*newPerson)
 
-	return &pb.PersonCreatedResponse{Id: newPerson.ID, Response: status}, nil
+	return &pb.CreatePersonResponse{Id: newPerson.ID, Response: status}, nil
 }
 
-func (s *Service) Get(ctx context.Context, request *pb.PersonRequest) (*pb.Person, error) {
+func (s *Service) Get(ctx context.Context, request *pb.GetPersonRequest) (*pb.GetPersonResponse, error) {
 	id := request.Id
 	dbResponse, err := dataAccess.Read(id)
 
@@ -43,20 +42,21 @@ func (s *Service) Get(ctx context.Context, request *pb.PersonRequest) (*pb.Perso
 		return nil, err
 	}
 
-	personResponse := &pb.Person{
-		Id:       dbResponse.ID,
-		Name:     dbResponse.FirstName,
-		LastName: dbResponse.LastName,
-		Email:    dbResponse.Email,
+	person := &pb.Person{
+		Id:        dbResponse.ID,
+		FirstName: dbResponse.FirstName,
+		LastName:  dbResponse.LastName,
+		Email:     dbResponse.Email,
 	}
 
-	return personResponse, nil
+	return &pb.GetPersonResponse{
+		Person: person,
+	}, nil
 }
 
 func (s *Service) Delete(ctx context.Context, request *pb.DeletePersonRequest) (*pb.DeletePersonResponse, error) {
 	id := request.Id
 	queryPerson, err := dataAccess.Read(id)
-	log.Println("id service: ", id)
 
 	if err != nil {
 		return nil, err
