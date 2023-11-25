44a69455b5752a94e2dbb68e05f403a6363a1aab
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-11-25 15:39:13 -0500

    chore: adds omitempty to struct tag

--- internal/person/models/models.go
+++ internal/person/models/models.go
@@ -1,7 +1,7 @@
 package models
 
 type Person struct {
-	ID        string `json:"id"`
+	ID        string `json:"id,omitempty"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
