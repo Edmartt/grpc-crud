693cdf06b20d469a690720be121f5254493c2546
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-12-02 19:50:44 -0500

    chore: calls start from http package for running http server as concurrent

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/edmartt/grpc-test/internal/database"
 	"github.com/edmartt/grpc-test/internal/server"
+	"github.com/edmartt/grpc-test/pkg/client/http"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	go http.Start("8080")
 	godotenv.Load()
 	database.InitMigrations()
 	server.StartServer()
